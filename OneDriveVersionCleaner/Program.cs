9023a5870206e6206be657bd9e5f2b64263a8cfe
György Balássy <balassy@users.noreply.github.com>
2018-11-11 04:20:50 +0100

    ADDED: First version of the application.

---
+++ OneDriveVersionCleaner/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Security;
+using Microsoft.SharePoint.Client;
+
+namespace OneDriveVersionCleaner
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      // ------------------------------------------------------------------------------------------------------------
+      // IMPORTANT: This is not production code, and it deletes files from your cloud storage. Use at your own risk!
+      // ------------------------------------------------------------------------------------------------------------
+
+      // TODO: Configure these values.
+      string webFullUrl = "https://TODO.sharepoint.com/personal/TODO_onmicrosoft_com";
+      string username = "TODO@TODO.onmicrosoft.com";
+      SecureString password = new NetworkCredential("", "TODO-ENTER-YOUR-APP-PASSWORD-HERE").SecurePassword;  // HACK :)
+
+      string[] folderRootRelativeUrls = {
+          "/Documents/subfolder1",
+          "/Documents/subfolder1/subfolder11",
+          "/Documents/subfolder1/subfolder11/subfolder111",
+          "/Documents/subfolder1/subfolder12",
+          "/Documents/subfolder2/subfolder21"
+      };
+
+      // --- End of configuration ---
+
+      ClientContext context = new ClientContext(webFullUrl)
+      {
+        Credentials = new SharePointOnlineCredentials(username, password)
+      };
+      context.Load(context.Web);
+      context.Load(context.Web.Lists);
+      context.Load(context.Web, web => web.ServerRelativeUrl);
+      context.ExecuteQuery();
+      WriteMessage($"Connected to: {context.Web.ServerRelativeUrl}", ConsoleColor.Green);
+
+      List list = context.Web.Lists.Single(l => l.Title == "Documents");
+      context.Load(list);
+      context.ExecuteQuery();
+      WriteMessage($"Number of files: {list.ItemCount}", ConsoleColor.Green);
+
+      foreach (string rootRelativeUrl in folderRootRelativeUrls)
+      {
+        WriteMessage($"\r\nFolder: {rootRelativeUrl}", ConsoleColor.Green);
+        ProcessFolder(context, list, rootRelativeUrl);
+      }
+
+      WriteMessage("\r\nDone.", ConsoleColor.Green);
+    }
+
+    private static void ProcessFolder(ClientContext context, List list, string rootRelativeUrl)
+    {
+      const int pageSize = 100;
+
+      Folder folder = context.Web.GetFolderByServerRelativeUrl(context.Web.ServerRelativeUrl + rootRelativeUrl);
+      context.Load(folder);
+      context.ExecuteQuery();
+
+      CamlQuery query = new CamlQuery
+      {
+        ViewXml = $@"<View>
+                       <RowLimit>{pageSize}</RowLimit>
+                       <Query>
+                         <Where>
+                           <Eq>
+                             <FieldRef Name='ContentType'/>
+                             <Value Type='Computed'>Document</Value>
+                           </Eq>
+                         </Where>
+                       </Query>
+                     </View>",
+        FolderServerRelativeUrl = folder.ServerRelativeUrl
+      };
+
+      bool hasMoreRecords = false;
+      int pageCount = 1;
+
+      do
+      {
+        WriteMessage($"\r\nPage: {pageCount}", ConsoleColor.White);
+        ListItemCollection items = list.GetItems(query);
+        context.Load(items);
+        context.ExecuteQuery();
+
+        ProcessItems(context, items);
+
+        hasMoreRecords = items.ListItemCollectionPosition != null;
+        query.ListItemCollectionPosition = items.ListItemCollectionPosition;
+
+        pageCount++;
+      } while (hasMoreRecords);
+    }
+
+    private static void ProcessItems(ClientContext context, ListItemCollection items)
+    {
+      foreach (ListItem item in items)
+      {
+        context.Load(item);
+        context.ExecuteQuery();
+        ProcessFile(context, item);
+      }
+    }
+
+    private static void ProcessFile(ClientContext context, ListItem item)
+    {
+      File file = item.File;
+
+      if (file != null)
+      {
+        context.Load(file);
+        context.Load(file.Versions);
+        context.ExecuteQuery();
+        long fileSize = file.Length;
+        int versionCount = file.Versions.Count;
+
+        if (versionCount > 0)
+        {
+          WriteMessage($"File: {file.Name,30}, Version count: {versionCount,5}, Size: {fileSize, 8}, Deleting versions...", ConsoleColor.Gray);
+          // TODO: file.Versions.DeleteAll();
+        }
+        else
+        {
+          WriteMessage($"File: {file.Name,30}, Version count: {versionCount,5}", ConsoleColor.DarkGray);
+        }
+      }
+    }
+
+    private static void WriteMessage(string message, ConsoleColor color)
+    {
+      Console.ForegroundColor = color;
+      Console.WriteLine(message);
+      Console.ResetColor();
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# OneDriveVersionCleaner
+# OneDrive / SharePoint Version Cleaner
+
 This sample console application uses the SharePoint .NET client-side object model (CSOM) to delete old versions of all files from multiple folders from your SharePoint or OneDrive for Business subscription.
+
+## Important!
+
+**This is not production code, and it deletes files from your cloud storage. Use at your own risk!**
+
+## How it works
+
+This console application executes the following steps:
+
+1. Connects to your OneDrive for Business or SharePoint site collection.
+2. Finds the `Documents` document library.
+3. Iterates through the specified subfolder paths in the document library.
+4. It runs CAML queries in every folder to retrieve the documents (files). In a single query maximum 100 documents are retrieved, and the query is executed again and again until all documents are processed.
+5. If a document has multiple versions, they are deleted.
+
+## How to use it
+
+To use this sample follow these steps:
+
+1. Clone this repository.
+2. Open the `OneDriveVersionCleanerSolution.sln` solution in Visual Studio (written and tested with VS 2017).
+3. At the top of the `Main` method specify the URL of your cloud storage, your username and password, and the paths to the folders you want to process. If you have multifactor authentication enabled (you do, right?) you need to create an application password first (check the blog post mentioned in the Read more section below).
+4. In line 124 uncomment the `file.Versions.DeleteAll()` call to actually delete the old versions.
+5. Run at your own risk.
+
+## Read more
+
+You can read the full story behind this code and other options to reduce the OneDrive storage space needed for your files on my [Delete old document versions from OneDrive for Business blog post](https://gyorgybalassy.wordpress.com/2018/11/11/delete-old-document-versions-from-sharepoint-onedrive/). 
+
+## About the author
+
+This sample was created by [György Balássy](https://linkedin.com/in/balassy).
